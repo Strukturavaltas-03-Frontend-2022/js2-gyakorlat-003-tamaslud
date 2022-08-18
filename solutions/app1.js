e896182dfd1d366a9e54cbac52e29d1f3fe548ce
tamaslud <tamaslud@yahoo.com>
2022-08-18 18:15:48 +0200

    tests ok

--- solutions/app1.js
+++ solutions/app1.js
@@ -2,6 +2,15 @@ const user = {
   firstName: 'John',
   lastName: 'Doe',
 };
+let {lastName='unknown', firstName='unknown', job='unknown'} = user; 
+
+
+
+export {
+  firstName,
+  lastName,
+  job,
+};
 
 //
 
@@ -10,3 +19,17 @@ const user = {
 //   lastName,
 //   job,
 // };
+/*
+## 1. feladat
+- A `solutions/app1.js` fájlban dolgozz!
+- Adott az alábbi objektum:
+```javascript
+    const user = {
+        firstName: 'John',
+        lastName: 'Doe'
+    };
+```
+- Mentsd el külön lastName, firstName és job változókba a fenti objektum 
+property-jeit destructuring használatával!
+- Ha valamelyik nincs megadva, akkor az érték legyen az "unknown" string!
+*/
--- solutions/app2.js
+++ solutions/app2.js
@@ -2,3 +2,29 @@ const user = {
   firstName: 'John',
   lastName: 'Doe',
 };
+
+let {lastName:l='unknown', firstName:f='unknown', job:j='unknown'} = user; 
+
+
+
+export {
+  f,
+  l,
+  j,
+};
+
+
+
+/*
+## 2. feladat
+- A `solutions/app2.js` fájlban dolgozz!
+- Adott az alábbi objektum:
+```javascript
+    const user = {
+        firstName: 'John',
+        lastName: 'Doe'
+    };
+```
+- Mentsd el külön `l`, `f` és `j` változókba a fenti objektum lastName, firstName és job property-jeit! 
+- Ha valamelyik nincs megadva, akkor az érték legyen az "unknown" string!
+*/
